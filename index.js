aece3eaea6c8874f725ce9d56538570d23f8b8cb
sajib hossen <sajibhossen560@gmail.com>
2025-11-06 21:11:38 +0600

    arrow function used

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 //  reversed string 
 
-let str = "Hello";
-let reversed = str.split('').reverse().join('')
+// let str = "Hello";
+// let reversed = str.split('').reverse().join('')
 
-console.log(reversed)
+// console.log(reversed)
+
+const reverseString = s => [...s].reverse().join('')
+
+console.log(reverseString("hello i am "))
